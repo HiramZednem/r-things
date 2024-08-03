84fa075cd0b5ec9a56093a9495777ec908625635
HiramZednem <hirammendez000@gmail.com>
2024-08-03 01:10:22 -0600

    introduction to data types

---
+++ hands_on_r/playing_cards.R
@@ -0,0 +1,29 @@
+# There exist 6 types of atomic vector
+# a helping function to know what type are we working is typeof()
+# Double -> number
+die <- c(1,2,3,4,5,6)
+typeof(die)
+# Integers 
+int <- c(1L,3L,5L)
+typeof(int)
+# Characters
+text <- c("hello", "world")
+typeof(text)
+# Bool
+logic <- c(TRUE, FALSE, F,T)
+typeof(logic)
+# Complex and Raw, very doubtful to use, but here are for the sake
+# Complex, imaginary tnumber, it store complex numbers
+comp <- c(1i,4i)
+typeof(comp)
+# Row -> bytes of data
+typeof(raw(3))
+
+# -------------------------------#
+
+hand <- c('ace', 'king', 'queen', 'jack', 'ten')
+
+
+
+
+
